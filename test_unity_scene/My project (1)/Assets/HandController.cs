f5c4976eb54a77e410e90f56c7537e3030803095
Arrobert2202 <robertaron15@yahoo.ro>
2023-12-17 21:12:14 +0200

    hands

--- test_unity_scene/My project (1)/Assets/HandController.cs
+++ test_unity_scene/My project (1)/Assets/HandController.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandController : MonoBehaviour
 {
-    public Camera externalCamera;
-    public float interactionDistance = 10f;
+    public float interactionDistance = 15f;
     Animator animator;
 
     void Start()
@@ -14,23 +13,29 @@ public class HandController : MonoBehaviour
 
     void Update()
     {
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = externalCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
+            Camera mainCamera = Camera.main;
 
-            if (Physics.Raycast(ray, out hit, interactionDistance))
+            if (mainCamera != null)
             {
-                Debug.Log("Hit object: " + hit.collider.gameObject.name);
-                Debug.Log("Hit object tag: " + hit.collider.tag);
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-                if (hit.collider.CompareTag("BarObject"))
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, interactionDistance))
                 {
-                    animator.SetTrigger("Active");
-                }
-                else
-                {
-                    Debug.Log("Hit object with incorrect tag.");
+                    Debug.Log("Hit object: " + hit.collider.gameObject.name);
+                    Debug.Log("Hit object tag: " + hit.collider.tag);
+
+                    if (hit.collider.CompareTag("BarObject"))
+                    {
+                        animator.SetTrigger("Active");
+                    }
+                    else
+                    {
+                        Debug.Log("Hit object with incorrect tag.");
+                    }
                 }
             }
         }
