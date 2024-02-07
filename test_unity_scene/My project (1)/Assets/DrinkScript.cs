f8d72fc6af7c17e6b8b7bf9b901385f6d29ca6c3
Arrobert2202 <robertaron15@yahoo.ro>
2024-02-07 23:07:41 +0200

    drinks

---
+++ test_unity_scene/My project (1)/Assets/DrinkScript.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkScript : MonoBehaviour
+{
+    private List<string> drinks = new List<string>();
+    public Material newMaterial;
+    private Rigidbody rb;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("WineBottle") || collision.collider.CompareTag("GreenBottle") || collision.collider.CompareTag("JDBottle") || collision.collider.CompareTag("BeerBottle"))
+        {
+            string drinkName = collision.collider.tag;
+            Debug.Log("Collided with: " + drinkName);
+            if (!drinks.Contains(drinkName))
+            {
+                drinks.Add(drinkName);
+            }
+        }
+
+        if (collision.collider.CompareTag("BarObject") && drinks.Count > 0)
+        { 
+            Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = newMaterial;
+            }
+        }
+
+        rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            Invoke("ReluareFizica", 1);
+        }
+    }
+
+    void ReluareFizica()
+    {
+        if (rb != null)
+        {
+            rb.isKinematic = false; // Reluarea efectului fizicii asupra obiectului
+        }
+    }
+    public void DisplayContents()
+    {
+        foreach (string drink in drinks)
+        {
+            Debug.Log("Shaker contains " + drink);
+        }
+    }
+}
--- test_unity_scene/My project (1)/Assets/HandController.cs
+++ test_unity_scene/My project (1)/Assets/HandController.cs
@@ -29,9 +29,9 @@ public class HandController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("BarObject"))
+        if (collision.collider.CompareTag("WineBottle") || collision.collider.CompareTag("GreenBottle") || collision.collider.CompareTag("JDBottle") || collision.collider.CompareTag("BeerBottle") || collision.collider.CompareTag("Shaker") || collision.collider.CompareTag("BarObject"))
         {
-            Debug.Log("Collision with BarObject detected");
+            //Debug.Log("Collision with BarObject detected");
 
             isCollidingWithBarObject = true;
         }
@@ -39,78 +39,9 @@ public class HandController : MonoBehaviour
 
     void OnCollisionExit(Collision collision)
     {
-        if (collision.collider.CompareTag("BarObject"))
+        if (collision.collider.CompareTag("WineBottle") || collision.collider.CompareTag("GreenBottle") || collision.collider.CompareTag("JDBottle") || collision.collider.CompareTag("BeerBottle"))
         {
             isCollidingWithBarObject = false;
         }
     }
 }
-
-
-
-// using UnityEngine;
-// using UnityEngine.XR.Interaction.Toolkit;
-// using UnityEngine.InputSystem;
-
-// public class HandController : MonoBehaviour
-// {
-//     public Camera externalCamera;
-//     public float interactionDistance = 10f;
-//     Animator animator;
-//     private XRGrabInteractable grabInteractable;
-
-//     void Start()
-//     {
-//         animator = GetComponent<Animator>();
-//         grabInteractable = GetComponent<XRGrabInteractable>();
-
-//         // Verifică dacă grabInteractable este nul înainte de a adăuga ascultători
-//         if (grabInteractable != null)
-//         {
-//             grabInteractable.onSelectEntered.AddListener(OnGrab);
-//             grabInteractable.onSelectExited.AddListener(OnRelease);
-//         }
-//         else
-//         {
-//             Debug.LogError("Componenta XRGrabInteractable nu a fost găsită pe acest obiect.");
-//         }
-//     }
-
-
-//     void Update()
-//     {
-//         // Logică pentru apucarea obiectului cu mouse
-//         if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
-//         {
-//             Ray ray = externalCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-//             RaycastHit hit;
-
-//             if (Physics.Raycast(ray, out hit, interactionDistance))
-//             {
-//                 Debug.Log("Hit object: " + hit.collider.gameObject.name);
-//                 Debug.Log("Hit object tag: " + hit.collider.tag);
-
-//                 if (hit.collider.CompareTag("BarObject"))
-//                 {
-//                     animator.SetTrigger("Active");
-//                 }
-//                 else
-//                 {
-//                     Debug.Log("Hit object with incorrect tag.");
-//                 }
-//             }
-//         }
-//     }
-
-//     // Funcție apelată când obiectul este apucat
-//     private void OnGrab(XRBaseInteractor interactor)
-//     {
-//         Debug.Log("Obiect apucat cu controller-ul!");
-//     }
-
-//     // Funcție apelată când obiectul este eliberat
-//     private void OnRelease(XRBaseInteractor interactor)
-//     {
-//         Debug.Log("Obiect eliberat cu controller-ul!");
-//     }
-// }
